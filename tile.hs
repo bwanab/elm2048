0013a2ae55922022987317d155fe081d4831111b
Bill Allen <bill@k2bea.org>
2014-05-26 21:25:00 -0400

    get rotations and all direction swiping working, except needs testing

--- tile.hs
+++ tile.hs
@@ -1,3 +1,8 @@
+-- Module Tile
+-- ( swipe
+-- , rotate
+-- ) where
+
 import Data.List
 
 data Tile = Empty | T2 | T4 | T8 | T16 | T32 | T64 | T128 | T256 | T512 | T1024 | T2048
@@ -25,15 +30,28 @@ swipe1 (x:xs) | x == Empty = swipe1 xs ++ [Empty]
 swipeRow :: [Tile] -> [Tile]
 swipeRow row = let l = iterate swipe1 row
                    l1 = group l
-                   l2 = dropWhile (\x -> length x == 1) (map (\x -> take 4 x) l1)
+                   l2 = dropWhile (\x -> length x == 1) (map (\x -> take 6 x) l1)
                    l3 = head (head l2)
                in
                    l3 ++ take (4 - (length l3)) (repeat Empty)
 
-swipe :: [[Tile]] -> [[Tile]]
-swipe rows = map (\row -> swipeRow row) rows
+data SwipeDirection = RightToLeft | TopToBottom | BottomToTop | LeftToRight
+
+swipe :: SwipeDirection -> [[Tile]] -> [[Tile]]
+swipe RightToLeft rows = map (\row -> swipeRow row) rows
+swipe BottomToTop rows = rotate TLeft (swipe RightToLeft (rotate TLeft rows))
+swipe LeftToRight rows = rotate Flip (swipe RightToLeft (rotate Flip rows))
+swipe TopToBottom rows = rotate TRight (swipe RightToLeft (rotate TRight rows))
 
 r = [[Empty, T2, Empty, T2],
      [T4   , Empty, T2, T2],
      [T2, Empty, Empty, T2],
      [Empty, T2, T4, T4]]
+
+
+data Rotation = TLeft | TRight | Flip
+
+rotate :: Rotation -> [[Tile]] -> [[Tile]]
+rotate TLeft rows = reverse rows
+rotate TRight rows = map (\l -> reverse l) rows
+rotate Flip rows = rotate TLeft (rotate TRight rows)
