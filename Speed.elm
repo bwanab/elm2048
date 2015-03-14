65b438bf32978abbdc75aaf1b6f5e99e1d2b5f7e
Bill Allen <bill@k2bea.org>
2015-03-14 17:12:12 -0400

    add a frame and don't add position distance (z) when out of frame

--- Speed.elm
+++ Speed.elm
@@ -1,6 +1,6 @@
 import Graphics.Element (..)
 import Graphics.Collage (group,rect,filled,toForm,collage,circle,move)
-import Color (lightGrey,red,blue)
+import Color (lightGrey,red,blue,lightBlue)
 import Text (plainText, fromString, rightAligned, defaultStyle, style, centered)
 import Signal (map2, map4,foldp)
 import Keyboard
@@ -10,10 +10,16 @@ import Time(..)
 import Random (Seed, float, generate, initialSeed)
 
 type alias V = {x : Float, y : Float , z : Float}
-type alias State = {position : V, speed : V, accel : V, seed: Seed}
+type alias F = {x : Float, y : Float , w : Float, h : Float}
+type alias State = {position : V, speed : V, accel : V, seed: Seed, frame: F}
 
 m : State
-m = {position = {x = 0, y = 0, z = 0}, speed = {x = 0, y = 0, z = 0}, accel = {x = 0, y = 0, z = 0}, seed = initialSeed 17890714}
+m = {
+  position = {x = 0, y = 0, z = 0},
+  speed = {x = 0, y = 0, z = 0},
+  accel = {x = 0, y = 0, z = 0},
+  seed = initialSeed 17890714,
+  frame = {x = 0, y = 0, w = 200, h = 200}}
 
 addV : V -> V -> V
 addV v1 v2 = {x = v1.x + v2.x, y = v1.y + v2.y, z = v1.z + v2.z }
@@ -23,6 +29,17 @@ addBuffeting val rand speed = val + speed * (rand - 0.5)
 
 testBuffet : Float -> Bool
 testBuffet r = 0 == ((round (r * 100)) % 2)
+
+testInFrame : V -> F -> Bool
+testInFrame position frame =
+    let
+        ww = frame.w / 2.0
+        hh = frame.h / 2.0
+    in
+      if (position.x > (frame.x + ww)) then False else
+      if (position.x < (frame.x - ww)) then False else
+      if (position.y > (frame.y + hh)) then False else
+      if (position.y < (frame.y - hh)) then False else True
 
 -- Update
 update : (Time, {x: Int, y: Int}, Bool, Bool) -> State -> State
@@ -37,7 +54,8 @@ update (t, {x,y}, forward, brake) state =
         (rand, newSeed) = generate (float 0 1) state.seed
         tB = testBuffet rand
         newPosition = {tPosition | x <- if tB then addBuffeting tPosition.x rand newSpeed.z else tPosition.x,
-                                   y <- if not tB then addBuffeting tPosition.y rand newSpeed.z else tPosition.y }
+                                   y <- if not tB then addBuffeting tPosition.y rand newSpeed.z else tPosition.y,
+                                   z <- if testInFrame tPosition m.frame then tPosition.z else state.position.z }
    in
         {state | speed <- newSpeed, position <- newPosition, accel <- newAccel, seed <- newSeed }
 
@@ -58,7 +76,9 @@ showV label v =
 
 display : Int -> Int -> State -> Element
 display w h m = collage w h [ rect (toFloat w) (toFloat h) |> filled lightGrey,
-                              circle 10 |> filled blue |> move (m.position.x, m.position.y) ]
+                              rect m.frame.w m.frame.h |> filled lightBlue |> move (m.frame.x, m.frame.y),
+                              circle 10 |> filled blue |> move (m.position.x, m.position.y)
+                            ]
 
 render : (Int, Int) -> State -> Element
 render (w,h) m = container w h middle (flow down [flow right [showV "Position" m.position,
